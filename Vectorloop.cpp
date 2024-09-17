64c766774f7f4c55791a509b6343555a34a35b7b
avanishkasar <151715253+avanishkasar@users.noreply.github.com>
2024-09-17 20:08:33 +0530

    Update Vectorloop.cpp

--- Vectorloop.cpp
+++ Vectorloop.cpp
@@ -4,31 +4,12 @@
 using namespace std;
 
 int main() {
-
-    vector<int> marks = { 85 , 90 , 99 , 97 , 60 };//start,stop,step
-    cout << "BEFORE = " ;
-    for (size_t i = 0; i < marks.size(); i++){
-     cout << marks[i] << " " ;
-    }
-    cout << endl ;
-    cout << "AFTER = " ;
-    for (size_t i = 0; i < marks.size(); i++){
-        marks[i] += 5 ;
-        if (marks[i] > 100)
-        {
-         marks[i] = 100;
-        }
-        cout << marks[i] << " " ;
-    }
-    cout << endl;
-    
-
-
-
-
-
-
-
-
-
+    int number;
+  int factorial = 1;
+  cin >> number ; 
+  for (int i = 1; i <= number; i++)
+  {
+    factorial *= i; 
+  }
+  cout << "factorial of " << number << "=" << factorial << endl;
 }
